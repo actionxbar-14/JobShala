598bc36428c9a311e21906eff0d0824edda7aed7
actionxbar-14 <ankitpaths144@gmail.com>
2025-07-07 10:51:37 +0530

    another change

--- JobShala/src/components/Auth.jsx
+++ JobShala/src/components/Auth.jsx
@@ -9,7 +9,7 @@ const AuthPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-[#001a26] text-white px-4">
       <div className="w-full max-w-md bg-white/5 backdrop-blur-md p-8 rounded-xl shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">
-          {isLogin ? "Login to JobBazar" : "Sign Up for JobBazar"}
+          {isLogin ? "Login to JobShala" : "Sign Up for JobShala"}
         </h2>
 
         <form className="flex flex-col gap-4">
